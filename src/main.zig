8f6803f581f992522abc6029292ba9f6eea7e762
philipsoitu <philip.soitu@gmail.com>
2025-03-03 13:49:36 -0500

    handles get "/"

--- src/main.zig
+++ src/main.zig
@@ -10,14 +10,37 @@ pub fn main() !void {
     });
     defer listener.deinit();
 
+    const allocator = std.heap.page_allocator;
+    const buff = try allocator.alloc(u8, 2048);
+    defer allocator.free(buff);
+
     const connection = try listener.accept();
     defer connection.stream.close();
 
+    _ = try connection.stream.read(buff);
     try stdout.print("client connected!\n", .{});
 
-    try success(connection);
+    var split = std.mem.split(u8, buff, " ");
+    const request_type = split.next();
+    const request_target = split.next();
+    try stdout.print("request_type: {?s}\n", .{request_type});
+    try stdout.print("request_target: {?s}\n", .{request_target});
+
+    if (std.mem.eql(u8, request_type.?, "GET")) {
+        if (std.mem.eql(u8, request_target.?, "/")) {
+            try success(connection);
+        } else {
+            try not_found(connection);
+        }
+    } else {
+        try not_found(connection);
+    }
 }
 
 pub fn success(connection: net.Server.Connection) !void {
     _ = try connection.stream.write("HTTP/1.1 200 OK\r\n\r\n");
+}
+
+pub fn not_found(connection: net.Server.Connection) !void {
+    _ = try connection.stream.write("HTTP/1.1 404 Not Found\r\n\r\n");
 }
