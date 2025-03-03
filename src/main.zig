197703b2782ea41c4364ecb040ec56e22983af9e
philipsoitu <philip.soitu@gmail.com>
2025-03-03 13:26:45 -0500

    socket works

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,23 @@
 const std = @import("std");
+const net = std.net;
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    const stdout = std.io.getStdOut().writer();
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    const address = try net.Address.resolveIp("127.0.0.1", 42069);
+    var listener = try address.listen(.{
+        .reuse_address = true,
+    });
+    defer listener.deinit();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    const connection = try listener.accept();
+    defer connection.stream.close();
 
-    try bw.flush(); // don't forget to flush!
+    try stdout.print("client connected!\n", .{});
+
+    try success(connection);
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+pub fn success(connection: net.Server.Connection) !void {
+    _ = try connection.stream.write("HTTP/1.1 200 OK\r\n\r\n");
 }
