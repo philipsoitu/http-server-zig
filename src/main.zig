23e500f002669f359e3096395aaa8363b3da67c7
philipsoitu <philip.soitu@gmail.com>
2025-03-08 11:59:50 -0500

    multithreading works

--- src/main.zig
+++ src/main.zig
@@ -8,33 +8,49 @@ pub fn main() !void {
         return;
     };
 
-    var server = try addr.listen(.{});
-    startServer(&server);
-}
+    const gpa = std.heap.page_allocator;
+    var thread_pool: std.Thread.Pool = undefined;
+    try std.Thread.Pool.init(&thread_pool, .{
+        .allocator = gpa,
+        .n_jobs = 4,
+    });
+    defer thread_pool.deinit();
 
-fn startServer(server: *net.Server) void {
+    var server = try addr.listen(.{});
+
     while (true) {
-        var connection = server.accept() catch |err| {
+        const connection = server.accept() catch |err| {
             std.debug.print("Accept error: {}\n", .{err});
             continue;
         };
-        defer connection.stream.close();
 
-        var read_buffer: [1024]u8 = undefined;
-        var http_server = http.Server.init(connection, &read_buffer);
-
-        var request = http_server.receiveHead() catch |err| switch (err) {
-            error.HttpConnectionClosing => continue,
-            else => |e| {
-                std.debug.print("Error receiving request head: {}\n", .{e});
-                continue;
-            },
-        };
-
-        handleRequest(&request) catch |err| {
-            std.debug.print("Request handling error: {}\n", .{err});
-        };
+        try thread_pool.spawn(struct {
+            fn run(conn: std.net.Server.Connection) void {
+                handleConnection(conn) catch |err| {
+                    std.log.err("Connection handling failed: {}\n", .{err});
+                };
+            }
+        }.run, .{connection});
     }
+}
+
+fn handleConnection(connection: std.net.Server.Connection) !void {
+    defer connection.stream.close();
+
+    var read_buffer: [1024]u8 = undefined;
+    var http_server = http.Server.init(connection, &read_buffer);
+
+    var request = http_server.receiveHead() catch |err| switch (err) {
+        error.HttpConnectionClosing => return,
+        else => |e| {
+            std.debug.print("Error receiving request head: {}\n", .{e});
+            return;
+        },
+    };
+
+    handleRequest(&request) catch |err| {
+        std.debug.print("Request handling error: {}\n", .{err});
+    };
 }
 
 fn handleRequest(request: *http.Server.Request) !void {
